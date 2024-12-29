5c6eaee99c9f30188bcf506def97cdc0f1cca818
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2024-12-29 15:26:05 +0300

    launch screen created

---
+++ Core Flow/Other/Core_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_FlowApp.swift
+//  Core Flow
+//
+//  Created by Victoria Samsonova on 29.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Core_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Core Flow/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Core Flow
+//
+//  Created by Victoria Samsonova on 29.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
