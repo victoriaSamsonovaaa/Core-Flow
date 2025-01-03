a5c8f61f47d190084d2af65403f4bd8fa610ea97
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-04 00:36:36 +0300

    creating account without errors

--- Core Flow/ViewModels/SignUpEmailViewViewModel.swift
+++ Core Flow/ViewModels/SignUpEmailViewViewModel.swift
@@ -15,26 +15,9 @@ class SignUpEmailViewViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var secondPassword: String = ""
     @Published var customMessage: String = ""
+    @Published var flag: Bool = true
 
     func signUp() {
-        let emailOutput = validateEmail(email: email)
-        guard !emailOutput.0 else {
-            customMessage = emailOutput.1
-            return
-        }
-
-        let passwordOutput = validatePassword(password: password)
-        guard !passwordOutput.0 else {
-            customMessage = passwordOutput.1
-            return
-        }
-
-        let secondPasswordOutput = comparePasswords(password: password, confirmPassword: secondPassword)
-        guard !secondPasswordOutput.0 else {
-            customMessage = secondPasswordOutput.1
-            return
-        }
-
         Task {
             do {
                 let returnedUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
@@ -49,28 +32,50 @@ class SignUpEmailViewViewModel: ObservableObject {
 
     func validateEmail(email: String) -> (Bool, String) {
         guard !email.isEmpty else {
+            flag = false
             return (false, "You can not leave email field empty. ")
         }
+
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return (emailPredicate.evaluate(with: email), emailPredicate.evaluate(with: email) ? "" : "Invalid email format")
+        let isValid = emailPredicate.evaluate(with: email)
+
+        if !isValid {
+            flag = false
+        }
+        
+        return (isValid, isValid ? "" : "Invalid email format")
     }
 
     func validatePassword(password: String) -> (Bool, String) {
         guard !password.isEmpty else {
-            return (false, "You can not leave password field empty. ")
+            flag = false
+            return (false, "You can not leave password field empty.")
         }
 
         let passwordRegEx = "(?=.*[0-9])(?=.*[A-Z])[0-9a-zA-Z]{6,15}"
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-        return (passwordPredicate.evaluate(with: password), passwordPredicate.evaluate(with: password) ? "" : "Password must contain at least one uppercase letter, one number and be between 6 and 15 characters long")
+        let isValid = passwordPredicate.evaluate(with: password)
+
+        if !isValid {
+            flag = false
+        }
+        
+        return (isValid, isValid ? "" : "Password must contain at least one uppercase letter, one number and be between 6 and 15 characters long")
     }
 
     func comparePasswords(password: String, confirmPassword: String) -> (Bool, String) {
         guard !password.isEmpty else {
-            return (false, "You should confirm password. ")
+            flag = false
+            return (false, "You should confirm password.")
         }
 
-        return (password == confirmPassword, password == confirmPassword ? "" : "Passwords do not match")
+        let passwordsMatch = (password == confirmPassword)
+
+        if !passwordsMatch {
+            flag = false
+        }
+
+        return (passwordsMatch, passwordsMatch ? "" : "Passwords do not match")
     }
 }
--- Core Flow/Views/SignUpEmailView.swift
+++ Core Flow/Views/SignUpEmailView.swift
@@ -22,7 +22,7 @@ struct SignUpEmailView: View {
                 .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
                 .padding(.bottom, 4)
             CustomTextField(placeholder: "Enter your Name", text: $viewModel.name)
-                .padding(.bottom, 50)
+                .padding(.bottom, 30)
 
             Text("Email")
                 .font(.custom("Cochin-Bold", size: 24))
@@ -34,7 +34,7 @@ struct SignUpEmailView: View {
                 }
 
             Text(emailOutput)
-                .padding(.bottom, 40)
+                .padding(.bottom, 20)
                 .font(.custom("Cochin-Bold", size: 20))
                 .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)).secondary)
 
@@ -47,7 +47,7 @@ struct SignUpEmailView: View {
                     passwordOutput = viewModel.validatePassword(password: newValue).1
                 }
             Text(passwordOutput)
-                .padding(.bottom, 40)
+                .padding(.bottom, 20)
                 .font(.custom("Cochin-Bold", size: 20))
                 .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)).secondary)
 
@@ -60,24 +60,25 @@ struct SignUpEmailView: View {
                     secondPasswordOutput = viewModel.comparePasswords(password: viewModel.password, confirmPassword: viewModel.secondPassword).1
                 }
             Text(secondPasswordOutput)
-                .padding(.bottom, 40)
+                .padding(.bottom, 20)
                 .font(.custom("Cochin-Bold", size: 20))
                 .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)).secondary)
 
+            Text(viewModel.customMessage)
+                .padding(.bottom, 8)
+                .font(.custom("Cochin-Bold", size: 24))
+                .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)).secondary)
+            
             Button("Create an account") {
                 viewModel.signUp()
             }
+            .disabled(viewModel.flag)
             .font(.custom("Cochin", size: 26))
             .foregroundStyle(Color(#colorLiteral(red: 0.9121661782, green: 0.8284091949, blue: 0.773633182, alpha: 1)))
             .frame(maxWidth: 370, maxHeight: 55)
             .background(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
             .cornerRadius(16)
             .padding(.bottom, 10)
-            
-            Text(viewModel.customMessage)
-                .padding(.bottom, 8)
-                .font(.custom("Cochin-Bold", size: 20))
-                .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)).secondary)
 
         }
         .padding()
