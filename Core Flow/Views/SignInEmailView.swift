b550e845507323c863d325681562f2f39d7d906b
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-08 23:51:13 +0300

    recreating signIn view

--- Core Flow/Views/SignInEmailView.swift
+++ Core Flow/Views/SignInEmailView.swift
@@ -11,37 +11,100 @@ struct SignInEmailView: View {
     @State private var viewModel = SignInEmailViewViewModel()
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
+            Text("Welcome back")
+                .font(.custom("Optima-Regular", size: 48))
+                .bold()
+                .foregroundStyle(Color.customBlue)
+                .padding(.top, 20)
+                .padding(.bottom, 22)
             
-            Text("Email")
-                .font(.custom("Cochin", size: 24))
-                .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
-                .padding(.bottom, 4)
-            CustomTextField(placeholder: "Enter your Email", text: $viewModel.email)
-                .padding(.bottom, 40)
+            VStack(alignment: .leading) {
+                
+                Text("Email")
+                    .font(.custom("Cochin", size: 24))
+                    .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                    .padding(.bottom, 4)
+                CustomTextField(placeholder: "Enter your Email", text: $viewModel.email)
+                    .padding(.bottom, 22)
+                
+                Text("Password")
+                    .font(.custom("Cochin", size: 24))
+                    .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                    .padding(.bottom, 4)
+                CustomSecureField(placeholder: "Enter your password", text: $viewModel.password)
+                    .padding(.bottom, 20)
+                
+            }
+            .padding()
             
-            Text("Password")
-                .font(.custom("Cochin", size: 24))
-                .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
-                .padding(.bottom, 4)
-            CustomSecureField(placeholder: "Enter your password", text: $viewModel.password)
-                .padding(.bottom, 40)
+            Button("Forgot password?") {
+                
+            }
+            .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+            .font(.custom("Cochin-Bold", size: 20))
+            .padding(.bottom, 30)
             
             Button("Sign In") {
                 
             }
             .font(.custom("Cochin", size: 26))
             .foregroundStyle(Color(#colorLiteral(red: 0.9121661782, green: 0.8284091949, blue: 0.773633182, alpha: 1)))
-            .frame(width: 360, height: 55)
+            .frame(width: 370, height: 55)
             .background(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
             .cornerRadius(16)
-            .padding(.bottom, 20)
+            .padding(.bottom, 50)
             
+            HStack {
+                Rectangle()
+                    .frame(width: 158, height: 1)
+                    .overlay(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                    .opacity(0.3)
+                Text("Or")
+                    .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                    .font(.custom("Cochin", size: 20))
+                Rectangle()
+                    .frame(width: 158, height: 1)
+                    .overlay(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                    .opacity(0.3)
+                }
+            
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image("facebook")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .scaledToFit()
+                }
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "apple.logo")
+                        .resizable()
+                        .frame(width: 25.25, height: 30)
+                        .scaledToFit()
+                        .tint(Color.black)
+                }
+                .padding()
+                
+                
+                Button {
+                    
+                } label: {
+                    Image("google")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .scaledToFit()
+                }
+                
+                Spacer()
+            }
         }
-        
-        .padding()
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color(#colorLiteral(red: 0.9121661782, green: 0.8284091949, blue: 0.773633182, alpha: 1)).opacity(0.6))
+
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.9121661782, green: 0.8284091949, blue: 0.773633182, alpha: 0.7578390731)))
     }
