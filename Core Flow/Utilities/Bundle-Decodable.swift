a7d2c32a8017c8e3bfb1adceb160f88ab8f72fa5
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-14 01:01:46 +0300

    exercise view

--- Core Flow/Utilities/Bundle-Decodable.swift
+++ Core Flow/Utilities/Bundle-Decodable.swift
@@ -9,6 +9,7 @@ import Foundation
 
 extension Bundle {
     func decode <T: Codable>(_ file: String) -> T {
+        
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
