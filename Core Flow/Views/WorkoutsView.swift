5e75bf8191dd9d80cdfac48cbb2b32099a7a01ae
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-15 19:13:53 +0300

    workouts view

--- Core Flow/Views/WorkoutsView.swift
+++ Core Flow/Views/WorkoutsView.swift
@@ -19,23 +19,64 @@ struct WorkoutsView: View {
         NavigationStack {
             ScrollView {
                 ForEach(workoutsByPart.muscle) { muscle in
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(muscle.exercises) { exercise in
-                                NavigationLink {
-                                    ExerciseView(exercise: exercise)
-                                } label: {
-                                    Image(systemName: "person")
+                    VStack(alignment: .leading) {
+                        Text(muscle.name)
+                            .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                            .font(.custom("Cochin-bold", size: 32))
+                            .padding(.vertical, 10)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack {
+                                ForEach(muscle.exercises) { exercise in
+                                    NavigationLink {
+                                        ExerciseView(exercise: exercise)
+                                    } label: {
+                                        VStack {
+                                            Image("cat_cow.jpg")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 200, height: 100)
+                                                .padding()
+                                            
+                                            VStack {
+                                                Text(exercise.workoutName)
+                                                    .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                                                    .font(.custom("Cochin-bold", size: 24))
+                                                
+                                                Text("\(exercise.workoutDifficulty) / 5")
+                                                    .foregroundStyle(Color(#colorLiteral(red: 0.09077811986, green: 0.09625732154, blue: 0.2869860828, alpha: 0.7636585884)))
+                                                    .font(.custom("Cochin-bold", size: 24))
+                                                
+                                            }
+                                            .padding(.vertical)
+                                            .frame(maxWidth: .infinity)
+                                            .background(.customBeige)
+                                        }
+                                        .clipShape(.rect(cornerRadius: 10))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(.customBlue)
+                                        )
+                                    }
                                 }
                             }
                         }
                     }
+                    .padding()
                 }
             }
-            .navigationTitle("Exercises")
-            .background(Color(#colorLiteral(red: 0.3328896165, green: 0.4141520262, blue: 0.3390924037, alpha: 1)))
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    Text("Select yuor exercises!")
+//                        .font(.largeTitle)
+//                        .fontDesign(.rounded)
+//                }
+//            }
+            .navigationTitle("Select yuor exercises!")
+            .navigationBarTitleTextColor(.customBlue)
         }
     }
+
 }
 
 #Preview {
