b553c5f4ef7ae55a6f71c415100b752e7f5af21a
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-17 12:56:35 +0300

    added favourite field to exercise

--- Core Flow/Models/ExerciseModel.swift
+++ Core Flow/Models/ExerciseModel.swift
@@ -5,13 +5,70 @@
 //  Created by Victoria Samsonova on 13.01.25.
 //
 
-import Foundation
+//import Foundation
+//import SwiftData
+//
+//
+//struct ExerciseModel: Codable, Identifiable {
+//    let id = UUID()
+//    let workoutName: String
+//    let workoutDescription: String
+//    let workoutExplanation: String
+//    let workoutImage: String
+//    let workoutDifficulty: Int
+//    let isFavourite: Bool
+//}
 
-struct ExerciseModel: Codable, Identifiable {
-    let id = UUID()
-    let workoutName: String
-    let workoutDescription: String
-    let workoutExplanation: String
-    let workoutImage: String
-    let workoutDifficulty: Int
+import SwiftUI
+import SwiftData
+
+@Model
+class ExerciseModel: Codable, Identifiable {
+    @Attribute(.unique) var id: UUID = UUID()
+    var workoutName: String
+    var workoutDescription: String
+    var workoutExplanation: String
+    var workoutImage: String
+    var workoutDifficulty: Int
+    var isFavourite: Bool = false 
+
+    init(id: UUID = UUID(), workoutName: String, workoutDescription: String, workoutExplanation: String, workoutImage: String, workoutDifficulty: Int, isFavourite: Bool = false) {
+        self.id = id
+        self.workoutName = workoutName
+        self.workoutDescription = workoutDescription
+        self.workoutExplanation = workoutExplanation
+        self.workoutImage = workoutImage
+        self.workoutDifficulty = workoutDifficulty
+        self.isFavourite = isFavourite
+    }
+
+    // Декодирование из JSON
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.workoutName = try container.decode(String.self, forKey: .workoutName)
+        self.workoutDescription = try container.decode(String.self, forKey: .workoutDescription)
+        self.workoutExplanation = try container.decode(String.self, forKey: .workoutExplanation)
+        self.workoutImage = try container.decode(String.self, forKey: .workoutImage)
+        self.workoutDifficulty = try container.decode(Int.self, forKey: .workoutDifficulty)
+    }
+
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(workoutName, forKey: .workoutName)
+        try container.encode(workoutDescription, forKey: .workoutDescription)
+        try container.encode(workoutExplanation, forKey: .workoutExplanation)
+        try container.encode(workoutImage, forKey: .workoutImage)
+        try container.encode(workoutDifficulty, forKey: .workoutDifficulty)
+        try container.encode(isFavourite, forKey: .isFavourite)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case workoutName
+        case workoutDescription
+        case workoutExplanation
+        case workoutImage
+        case workoutDifficulty
+        case isFavourite
+    }
 }
--- Core Flow/Other/Core_FlowApp.swift
+++ Core Flow/Other/Core_FlowApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Firebase
 import FirebaseAuth
+import SwiftData
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -25,5 +26,6 @@ struct Core_FlowApp: App {
         WindowGroup {
             FirstBaseView()
         }
+        .modelContainer(for: ExerciseModel.self)
     }
 }
