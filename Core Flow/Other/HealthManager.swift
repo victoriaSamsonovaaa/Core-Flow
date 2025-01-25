9edf1d5e4a442626002162c489f60a8e871d36ce
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-25 23:43:46 +0300

    beginning of healthkit

---
+++ Core Flow/Other/HealthManager.swift
@@ -0,0 +1,16 @@
+//
+//  HealthManager.swift
+//  Core Flow
+//
+//  Created by Victoria Samsonova on 25.01.25.
+//
+
+import Foundation
+import HealthKit
+
+class HealthManager: ObservableObject {
+    
+    init() {
+        let steps = HKQuantityType(.stepCount)
+    }
+}
--- Core Flow/Views/HomeView.swift
+++ Core Flow/Views/HomeView.swift
@@ -9,12 +9,51 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Text("okkkk")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    VStack {
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("Calories")
+                                .font(.callout)
+                                .bold()
+                                .foregroundStyle(.red)
+                            Text("100 kcal")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("Active")
+                                .font(.callout)
+                                .bold()
+                                .foregroundStyle(.green)
+                            Text("52 mins")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("Stand")
+                                .font(.callout)
+                                .bold()
+                                .foregroundStyle(.blue)
+                            Text("8 hours")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(#colorLiteral(red: 0.3328896165, green: 0.4141520262, blue: 0.3390924037, alpha: 1)))
-        .edgesIgnoringSafeArea(.all)
+      //  .background(Color(#colorLiteral(red: 0.3328896165, green: 0.4141520262, blue: 0.3390924037, alpha: 1)))
+    //    .edgesIgnoringSafeArea(.all)
     }
 }
 
