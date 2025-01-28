536c9b7105c4de8961894146899aee4df20cf12c
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-28 18:45:09 +0300

    profile view

---
+++ Core Flow/Views/ProfileRowView.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileRowView.swift
+//  Core Flow
+//
+//  Created by Victoria Samsonova on 28.01.25.
+//
+
+import SwiftUI
+
+struct ProfileRowView: View {
+    let imageName: String
+    let title: String
+    let tint: Color
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(systemName: imageName)
+                .imageScale(.large)
+                .foregroundStyle(tint)
+            
+            Text(title)
+                .font(.subheadline)
+                .foregroundStyle(.customBlue)
+        }
+    }
+}
+
+//#Preview {
+//    ProfileRowView()
+//}
