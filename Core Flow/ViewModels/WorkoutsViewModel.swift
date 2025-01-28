16dac0781f891dba7342324392c73711d59336c4
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-28 23:41:29 +0300

    addToFav improved

--- Core Flow/ViewModels/WorkoutsViewModel.swift
+++ Core Flow/ViewModels/WorkoutsViewModel.swift
@@ -10,7 +10,12 @@ import SwiftData
 
 class WorkoutsViewModel: ObservableObject {
     
-    func addToFavourites() {
-        
+    func addToFavourites(exercise: ExerciseModel) async throws {
+        let currAuthUser = try AuthenticationManager.shared.getAuthenticatedUser()
+        print("got user")
+        let dbUser = try await UserManager.shared.getUser(userId: currAuthUser.uid)
+        print("got dbUser")
+        try await UserManager.shared.addToFavourite(exercise: exercise, user: dbUser)
+        print("added")
     }
 }
